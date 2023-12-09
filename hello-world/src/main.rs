dae8b1b6a6eb2545e28e0b6327034396a17ae8b0
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2023-12-09 19:30:36 +0700

    add types and variables

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,9 +1,14 @@
+mod sh;
 #[allow(dead_code)]
 #[allow(unused_imports)]
 
 use std::mem;
 
-fn main() {
+// constant
+const MEANING_OF_LIFE: u8 = 42; // no fixed address
+static Z: i32 = 123; // fixed address
+
+fn fundamental_data_types() {
     let a: u8 = 123; // 8-bit unsigned integer, 0 to 255, immutable
     println!("a = {}", a);
 
@@ -40,4 +45,65 @@ fn main() {
 
     let g: bool = false; // or true
     println!("g = {}, size = {} bytes", g, mem::size_of_val(&g));
+}
+
+fn operators() {
+    // arithmetic
+    let mut a = 2 + 3 * 4; // +-*/
+    println!("{}", a);
+
+    a = a + 1;
+    a -= 2; // -= += *= /= %=
+    println!("remainder of {} / {} = {}", a, 3, (a % 3));
+
+    let a_cubed = i32::pow(a, 3);
+    println!("{} cubed is {}", a, a_cubed);
+
+    let b = 2.5;
+    let b_cubed = f64::powi(b, 3); // powi for integer, powf for float
+    let b_to_pi = f64::powf(b, std::f64::consts::PI);
+    println!("{} cubed = {}, {}^pi = {}", b, b_cubed, b, b_to_pi);
+
+    // bitwise
+    let c = 1 | 2; // | OR & AND ^ XOR ! NOR
+                   // 01 OR 10 = 11 == 3_10
+    println!("1|2 = {}", c);
+
+    let two_to_10 = 1 << 10; // >>
+    println!("2^10 = {}", two_to_10);
+
+    // logical
+    let pi_less_4 = std::f64::consts::PI < 4.0; // true
+    let x = 5;
+    let x_is_5 = x == 5; // true
+}
+
+fn scope_and_shadowing() {
+    let a = 123;
+
+    {
+        let b = 456;
+        println!("inside, b = {}", b);
+
+        let a = 777;
+        println!("inside, a = {}", a);
+    }
+
+    println!("outside, a = {}", a);
+    // println!("outside, b = {}", b); // error: cannot find value `b` in this scope
+}
+
+fn control_flow() {
+    let temp = 25;
+
+    if temp > 30 {
+        println!("really hot outside");
+    }
+}
+
+fn main() {
+    fundamental_data_types();
+    operators();
+    scope_and_shadowing();
+    sh::stack_and_heap();
 }
